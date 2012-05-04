57c939a2f35810354adbe339389d71fe750094f2
dryes <joswiseman@gmail.com>
2012-05-04 07:14:21 +0200

    todo

--- rescepy/srr.py
+++ rescepy/srr.py
@@ -16,6 +16,8 @@
 # You should have received a copy of the GNU General Public License
 # along with rescepy.  If not, see <http://www.gnu.org/licenses/>.
 
+##TODO: verify srr
+
 import os,subprocess
 
 class SRR:
